83b1801be69188bfa23192eb1b469a724adce62a
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-01-13 05:14:02 +0100

    Add cancelable signIn method

--- src/Auth/index.ts
+++ src/Auth/index.ts
@@ -8,26 +8,34 @@ import {
 } from 'mainProcess/electron';
 import {
   openLink,
+  isDev,
+  changeAnalyticsUser,
   refreshAuthInOtherWindows,
 } from 'mainProcess';
-
-export const authState = new EventEmitter();
+import { timeout } from 'utils';
 
 export type AuthInfo = { user?: MagicUserMetadata, isLoading?: boolean };
-
 export type { MagicUserMetadata };
 
+export const authState = new EventEmitter();
 export let authInfo: AuthInfo = { isLoading: true };
+
+const magicAPIKey = isDev ? 'pk_test_2AE829E9A03C1FA0' : 'pk_live_C99F68FD8F927F2E';
+const magic = new Magic(magicAPIKey);
+
+let signInCancelHandle: (() => void) | undefined = undefined;
+
+authState.on('changed', (auth: AuthInfo) => {
+  const userID = auth?.user?.publicAddress || undefined;
+  // TODO: Test segment usedID aliasing again, with the whole sign-in flow
+  // changeAnalyticsUser(userID);
+});
+
+refreshAuth();
 
 function generateSessionID() {
   return encodeURIComponent(crypto.randomBytes(64).toString('base64'));
 };
-
-function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const magic = new Magic('pk_test_2AE829E9A03C1FA0');
 
 export async function signOut() {
   try {
@@ -41,44 +49,36 @@ export async function signOut() {
   }
 }
 
-let signInInProgressCancel: (() => void) | undefined = undefined;
-
 export function cancelSignIn() {
-  signInInProgressCancel?.();
+  signInCancelHandle?.();
 }
 
 export async function signIn(email: string) {
   cancelSignIn();
 
-  let resolveHandle: any;
-
-  let isCanceled = false;
-
-  function getIsCancelled() {
-    return isCanceled;
-  }
+  let rejectHandle: (reason?: any) => void;
+  let isCancelled = false;
 
   const cancelableSignIn = new Promise<void>(async (resolve, reject) => {
+    rejectHandle = reject;
 
-    resolveHandle = resolve;
-
-    const url = 'https://dev.usedevbook.com/auth';
-    // const url = 'http://localhost:3002/auth';
+    const url = isDev ? 'https://dev.usedevbook.com/auth' : 'https://api.usedevbook.com/auth';
 
     const sessionID = generateSessionID();
 
     const params = querystring.encode({
       email,
+      ...isDev && { test: 'true' },
     });
 
     await openLink(`${url}/signin/${sessionID}?${params}`);
 
     let credential: string | undefined = undefined;
 
-    const requestLimit = 60 * 15;
+    const requestLimit = 15 * 60;
 
     for (let i = 0; i < requestLimit; i++) {
-      if (getIsCancelled()) {
+      if (isCancelled) {
         break;
       }
 
@@ -98,31 +98,27 @@ export async function signIn(email: string) {
 
       } catch (error) {
         if (error.response?.status !== 404) {
-          // console.error(error);
           break;
         }
       }
       await timeout(1000);
     }
 
-    if (getIsCancelled()) {
-      return resolve();
+    if (isCancelled) {
+      return reject({ message: 'Sign in was cancelled' });
     }
 
-    if (!credential && !getIsCancelled()) {
-      return reject();
+    if (!credential && !isCancelled) {
+      return reject({ message: 'Getting credential for sign in timed out' });
     }
 
     try {
       const didToken = await magic.auth.loginWithCredential(credential);
       const userMetadata = await magic.user.getMetadata()
-      console.log('public address', userMetadata.publicAddress);
 
       await axios.post(`${url}/signin`, {
         didToken,
       });
-
-      // await aliasAnalyticsUser(userMetadata.publicAddress);
 
       authInfo = { user: userMetadata, isLoading: false };
       authState.emit('changed', authInfo);
@@ -131,19 +127,22 @@ export async function signIn(email: string) {
     } catch (error) {
       console.error(error);
     }
+
     return resolve();
   });
 
-  signInInProgressCancel = () => {
-    isCanceled = true;
-    resolveHandle();
+  signInCancelHandle = () => {
+    rejectHandle({ message: 'Sign in was cancelled' });
+    isCancelled = true;
   };
 
   return cancelableSignIn;
 }
 
-
 export async function refreshAuth() {
+  authInfo = { isLoading: true };
+  authState.emit('changed', authInfo);
+
   try {
     const isUserSignedIn = await magic.user.isLoggedIn();
     if (isUserSignedIn) {
@@ -156,5 +155,3 @@ export async function refreshAuth() {
   }
   authState.emit('changed', authInfo);
 }
-
-refreshAuth();
