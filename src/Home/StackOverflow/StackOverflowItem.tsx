533b0c4675307f13f7fe6f2aa68cd82465b92285
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2021-01-15 17:43:20 +0100

    Revert back to the old padding

--- src/Home/StackOverflow/StackOverflowItem.tsx
+++ src/Home/StackOverflow/StackOverflowItem.tsx
@@ -19,7 +19,6 @@ import StackOverflowBody from './StackOverflowBody';
 const Container = styled.div<{ isFocused?: boolean }>`
   width: 100%;
   max-width: 100%;
-  padding-bottom: 30px;
   margin-bottom: 30px;
 
   display: flex;
