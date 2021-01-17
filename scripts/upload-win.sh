0c43e0236dbde8292b54ae91cec7bdf2696ab517
ValentaTomas <valenta.and.thomas@gmail.com>
2021-01-17 22:40:44 +0100

    Fix win upload script

--- scripts/upload-win.sh
+++ scripts/upload-win.sh
@@ -1,5 +1,3 @@
-BUCKET=devbook-publish
-
-gsutil cp gs://$BUCKET/latest.yml gs://$BUCKET/archive/$(date +%Y-%M-%d-%H%M)-latest.yml
-
-echo Devbook-*.exe latest.yml | tr [:space:] '\n' | xargs -n 1 -I{} gsutil -h "Cache-Control:no-cache, max-age=0" cp dist/{} gs://$BUCKET
+export BUCKET="devbook-publish"
+gsutil cp "gs://$BUCKET/latest.yml" "gs://$BUCKET/archive/$(date +%Y-%M-%d-%H%M)-latest.yml"
+echo "Devbook-*.exe latest.yml" | tr [:space:] "\n" | xargs -n 1 -I{} gsutil -h "Cache-Control:no-cache, max-age=0" cp "dist/{}" "gs://$BUCKET/"
