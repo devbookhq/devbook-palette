00b6b0f47e81d03ce45bcdd9e0624138b6f884de
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-01-17 21:50:52 +0100

    Fix publishing bucket name

--- scripts/upload-win.sh
+++ scripts/upload-win.sh
@@ -1,5 +1,5 @@
-BUCKET=devbook-test
+BUCKET=devbook-publish
 
-gsutil cp gs://$BUCKET/latest.yml gs://$BUCKET/archive/$(date +%Y-%M-%d-%H%M)-latest-mac.yml
+gsutil cp gs://$BUCKET/latest.yml gs://$BUCKET/archive/$(date +%Y-%M-%d-%H%M)-latest.yml
 
 echo Devbook-*.exe latest.yml | tr [:space:] '\n' | xargs -n 1 -I{} gsutil -h "Cache-Control:no-cache, max-age=0" cp dist/{} gs://$BUCKET
