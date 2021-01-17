f032383c714ab8c76d988fc1e8b7813d440cb485
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2021-01-17 17:28:51 +0100

    When user is selecting a global shortcut in the Preferences view the text now says 'Option' instead of 'Alt' on macOS

--- src/Preferences/Pages/GeneralPreferences.tsx
+++ src/Preferences/Pages/GeneralPreferences.tsx
@@ -83,18 +83,19 @@ function GeneralPreferences() {
               <Description>A shortcut that you press to display Devbook.</Description>
             </Text>
             <StyledSelect value={selectedShortcut} onChange={e => handleShortcutChange(e.target.value)}>
-              <option value="Alt+Space">Alt+Space</option>
               <option value="Control+Space">Control+Space</option>
               <option value="Shift+Space">Shift+Space</option>
               {electron.remote.process.platform === 'darwin' &&
                 <>
+                  <option value="Alt+Space">Option+Space</option>
                   <option value="Command+Space">Command+Space</option>
                   <option value="Command+Shift+Space">Command+Shift+Space</option>
-                  <option value="Command+Alt+Space">Command+Alt+Space</option>
+                  <option value="Command+Alt+Space">Command+Option+Space</option>
                 </>
               }
               {electron.remote.process.platform === 'linux' &&
                 <>
+                  <option value="Alt+Space">Alt+Space</option>
                   <option value="Control+Shift+Space">Control+Shift+Space</option>
                   <option value="Control+Alt+Space">Control+Alt+Space</option>
                 </>
