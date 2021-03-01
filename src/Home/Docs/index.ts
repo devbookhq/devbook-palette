48f9db12f01c6086bad403a07108a9703c057d91
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-03-01 22:48:35 +0100

    Remove redundant newline

--- src/Home/Docs/index.ts
+++ src/Home/Docs/index.ts
@@ -1,4 +1,3 @@
 export { default as DocSearchResultItem } from './SearchResultItem';
 export { default as DocPage } from './DocPage';
 export { default as DocsFilterModal } from './DocsFilterModal';
-
