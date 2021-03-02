00a1902b49b1e011c4891c6a24fe238d5edfdc28
Tomáš Valenta <valenta.and.thomas@gmail.com>
2021-03-02 18:30:44 +0100

    Stop sorting search history

--- src/Home/SearchHistory/historyStore.ts
+++ src/Home/SearchHistory/historyStore.ts
@@ -3,7 +3,6 @@ import debounce from 'utils/debounce';
 
 interface HistoryEntry {
   query: string;
-  occurence: number;
 }
 
 class HistoryStore {
@@ -13,7 +12,7 @@ class HistoryStore {
 
   private static readonly searchHistoryEntriesStoreName = 'searchHistoryEntries';
 
-  private static readonly maxHistorySize = 200;
+  private static readonly maxHistorySize = 20;
   private static entries = HistoryStore.store.get(HistoryStore.searchHistoryEntriesStoreName, []) as HistoryEntry[];
   static readonly saveDebouncedQuery = debounce(HistoryStore.saveQuery, 5000);
 
@@ -27,23 +26,17 @@ class HistoryStore {
   private static saveQuery(query: string) {
     const index = HistoryStore.entries.findIndex(e => e.query === query);
     const entry = HistoryStore.entries[index];
+
     if (entry) {
-      entry.occurence = entry.occurence + 1;
       HistoryStore.entries.push(HistoryStore.entries.splice(index, 1)[0]);
     } else {
-      const length = HistoryStore.entries.push({ query, occurence: 1 });
+      const length = HistoryStore.entries.push({ query });
       if (length > HistoryStore.maxHistorySize) {
         HistoryStore.entries.splice(0, length - HistoryStore.maxHistorySize);
       }
     }
 
-    HistoryStore.entries = [
-      ...HistoryStore.entries.slice(0, -4).sort((a, b) => a.occurence - b.occurence),
-      ...HistoryStore.entries.slice(-4),
-    ];
-
     HistoryStore.store.set(HistoryStore.searchHistoryEntriesStoreName, HistoryStore.entries);
-    return HistoryStore.queries;
   }
 
   private constructor() { }
