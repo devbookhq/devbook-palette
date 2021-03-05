7b2b26b7276bbe76c89e3dcd18992f252b40b324
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2021-03-05 23:40:21 +0100

    Use @devbookhq/splitter insted of @devbookhq/react-split

--- src/newsrc/Board/Split.tsx
+++ src/newsrc/Board/Split.tsx
@@ -2,7 +2,7 @@ import React  from 'react';
 import styled from 'styled-components';
 import { observer } from 'mobx-react-lite';
 
-import ReactSplit, { SplitDirection as ReactSplitDirection } from '@devbookhq/react-split';
+import ReactSplit, { SplitDirection as ReactSplitDirection } from '@devbookhq/splitter';
 
 import Tile from 'newsrc/Board/Tile';
 import {
