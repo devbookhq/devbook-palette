5663984a16b51ed4bf2f6a4a46e20edc60721ec0
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2021-03-26 11:48:19 +0100

    Improve deploy script formatting

--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -28,36 +28,56 @@ if [ -z "$CLOUDFLARE_ACCOUNT_ID" ]; then
 fi
 
 
-read -p "🚨 You are about to publish client v$VERSION-$BUNDLE. Press enter to continue."
+read -p "🚨 You are about to deploy client v$VERSION-$BUNDLE. Press enter to continue."
 
 gsutil -m -h "Cache-Control:no-cache, max-age=0" cp -r $BUILD_PATH/ $BUCKET_URL/$VERSION
 
-echo "✅ Client v$VERSION -$BUNDLE uploaded to storage at URL $BUCKET_URL/$VERSION."
+echo "\n\n>>> ✅ Client v$VERSION -$BUNDLE uploaded to storage at URL $BUCKET_URL/$VERSION."
 
-echo "🗑 Purging CDN cache..."
+echo "\n\n"
+read -p ">>> 🗑 Will purge the CDN cache. Press enter to continue."
 
 # Purge CDN cache so the new version is available immediately.
-success=`curl "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"\
+response=`curl "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"\
   -X POST\
   -H "Content-Type: application/json"\
   -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL"\
   -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY"\
-  -d '{"purge_everything": true}' | jq -r '.success'`
+  -d '{"purge_everything": true}'`
 
-echo "Was purge successful? $success"
+echo === RESPONSE
+echo $response
+echo ===
+
+success=`echo $response | jq -r '.success'`
+echo "Was the cache purge successful? $success"
 if [ $success = "false" ]; then
+  echo "Failed to purge the cache. Exiting."
   exit 1
 fi
 
-echo "💾 Saving the latest bundle version..."
+echo "\n\n"
+read -p ">>> 💾 Will save the latest bundle version. Pres enter to continue."
 
 # Save the latest bundle version into KV in Cloudflare.
 NAMESPACE_ID="6c08f450925342c3b0d066b77eedbf25"
-curl "https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/$NAMESPACE_ID/values/${VERSION}"\
+response=`curl "https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/$NAMESPACE_ID/values/${VERSION}"\
   -X PUT\
   -H "Content-Type: text/plain"\
   -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL"\
   -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY"\
-  --data "$BUNDLE"
+  --data "$BUNDLE"`
 
+echo === RESPONSE
+echo $response
+echo ===
+
+success=`echo $response | jq -r '.success'`
+echo "Was the bundle update successful? $success"
+if [ $success = "false" ]; then
+  echo "Failed to purge the cache. Exiting."
+  exit 1
+fi
+
+echo "\n\n ✅ The client v$VERSION-$BUNDLE is deployed"
 
