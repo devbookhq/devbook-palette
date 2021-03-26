de85062e16a5ee6a10c52f597e354afeefc25ee8
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2021-03-26 11:25:17 +0100

    Make the build and deploy scripts idiot-self proof

--- scripts/bundle-client.sh
+++ scripts/bundle-client.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 VERSION=`cat package.json | jq -r '.version'`
-# This is an env var that React uses to prefix all assets imports.
-PUBLIC_URL=https://client.usedevbook.com/$VERSION
-BUNDLE=`openssl rand -hex 6`
+PUBLIC_URL=https://client.usedevbook.com/$VERSION # This is an env var that React uses to prefix all assets imports.
+BUNDLE=`git rev-parse --short HEAD` # Bundle is a hash value of the current commit.
 BUILD_PATH=build/client/$VERSION-$BUNDLE
 
 rm -rf build/client
 
 echo "PUBLIC_URL=$PUBLIC_URL\nBUILD_PATH=$BUILD_PATH\nREACT_APP_BUNDLE=$BUNDLE\nREACT_APP_VERSION=$VERSION"> .env.production
 
-echo "Bundling client v$VERSIO-$BUNDLE with PUBLIC_URL set to '$PUBLIC_URL'..."
+echo "Bundling client v$VERSION-$BUNDLE with PUBLIC_URL set to '$PUBLIC_URL'..."
 
 npx craco build
 
 echo "✅ Client  bundle v$VERSION-$BUNDLE done"
+
--- scripts/deploy-client.sh
+++ scripts/deploy-client.sh
@@ -1,26 +1,63 @@
 #!/bin/sh
 
-BUNDLE=$1
+set -e
+
+BUNDLE=`git rev-parse --short HEAD` # Bundle is a hash value of the current commit.
 BUCKET_URL="gs://testing-js-bundle"
 VERSION=`cat package.json | jq -r '.version'`
 BUILD_PATH=build/client/$VERSION-$BUNDLE
+
+if [ -z "$CLOUDFLARE_AUTH_EMAIL" ]; then
+  echo "CLOUDFLARE_AUTH_EMAIL env var is empty"
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_AUTH_KEY" ]; then
+  echo "CLOUDFLARE_AUTH_KEY env var is empty"
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_ZONE_ID" ]; then
+  echo "CLOUDFLARE_ZONE_ID env var is empty"
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_ACCOUNT_ID" ]; then
+  echo "CLOUDFLARE_ACCOUNT_ID env var is empty"
+  exit 1
+fi
+
 
 read -p "🚨 You are about to publish client v$VERSION-$BUNDLE. Press enter to continue."
 
 gsutil -m -h "Cache-Control:no-cache, max-age=0" cp -r $BUILD_PATH/ $BUCKET_URL/$VERSION
 
-echo "✅ Client v$VERSION uploaded to storage."
+echo "✅ Client v$VERSION -$BUNDLE uploaded to storage at URL $BUCKET_URL/$VERSION."
 
 echo "🗑 Purging CDN cache..."
 
 # Purge CDN cache so the new version is available immediately.
-success=`curl -s https://client.usedevbook.com/__purge_cache?zone=526615871424790820a10de58e0359ee | jq -r '.success'`
+success=`curl "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"\
+  -X POST\
+  -H "Content-Type: application/json"\
+  -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL"\
+  -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY"\
+  -d '{"purge_everything": true}' | jq -r '.success'`
 
 echo "Was purge successful? $success"
+if [ $success = "false" ]; then
+  exit 1
+fi
 
 echo "💾 Saving the latest bundle version..."
 
 # Save the latest bundle version into KV in Cloudflare.
-curl -X PUT -H "Content-Type: application/json" -d '{"version":"'$VERSION'", "bundle":"'$BUNDLE'"}' https://client.usedevbook.com/__latest-bundle
+NAMESPACE_ID="6c08f450925342c3b0d066b77eedbf25"
+curl "https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/$NAMESPACE_ID/values/${VERSION}"\
+  -X PUT\
+  -H "Content-Type: text/plain"\
+  -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL"\
+  -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY"\
+  --data "$BUNDLE"
 
 
